8e1bd9f6a982fcebfc2a3813104a3dde410a40d4
sunye <franksunye@hotmail.com>
2024-03-08 18:18:07 +0800

    more env settings.

--- shared/config/auth0-configuration.js
+++ shared/config/auth0-configuration.js
@@ -1,11 +1,11 @@
 // auth0-configuration.js
 const auth0Settings = {
-    domain: 'dev-ho4e6glpvc4ux5uw.us.auth0.com',
-    clientId: 'PwbIHMy1VzvrNMWGeEyrOXPR5P3RzOwV',
-    clientSecret: 'Nk9VEZoxvm66qeEPbx-mizAXe9poGD8CX7aEYtM-yirULl2i9ebo_x1nU5T6iJ4V',
-    callbackUrl: 'http://localhost:3000/callback',
-    logoutUrl: 'http://localhost:3000'
-  };
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackUrl: process.env.AUTH0_CALLBACK_URL,
+  logoutUrl: process.env.AUTH0_LOGOUT_URL
+ };
   
   module.exports = auth0Settings;
   
